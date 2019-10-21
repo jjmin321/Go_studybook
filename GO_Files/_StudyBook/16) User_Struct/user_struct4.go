4a5167d19c8b0f47524e19a21219d6fead20b06d
제정민 <jejeongmin@jejeongmin-ui-MacBookAir.local>
2019-10-21 16:53:47 +0900

    src

--- GO_Files/_StudyBook/16) User_Struct/user_struct4.go
+++ GO_Files/_StudyBook/16) User_Struct/user_struct4.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+type shoppingBasket struct {
+	cnt   int
+	price int
+}
+
+func (b shoppingBasket) purchase() int {
+	return b.cnt * b.price
+}
+
+//원본 수정(참조 형식)
+func (b *shoppingBasket) rePurchase(cnt, price int) {
+	b.cnt += cnt
+	b.price += price
+}
+
+/* 원본 수정 X (값 전달 방식)
+func (b *shoppingBasket) rePurchase(cnt, price int) {
+	b.cnt += cnt
+	b.price += price
+}
+*/
+
 func main() {
 	//GO -> 객체 지향 타입을 구조체로 정의한다. (클래스, 상속 개념 없음)
 	//객체 지향 -> 클래스(속성 : 멤버변수, 기능(상태 : 메소드)) : 코드의 재사용성, 코드의 관리가 용이, 신뢰성이 높은 프로그래밍
@@ -10,5 +34,16 @@ func main() {
 	//사용자 정의 타입 : 구조체, 인터페이스, 기본 타입(int, float, string...) , 함수
 	//구조체와 -> 메소드 연결을 통해서 타 언어의 클래스 형식처럼 사용 가능하다 (객체지향이 맞다)
 	//클래스는 구조체다. 구조체는 클래스가 아니다. 클래스는 구조체와 메소드의 연결과 같다.
+
+	//리시버 메소드 전달(값, 참조) 형식
+	//함수는 기본적으로 값 호출 -> 변수의 값이 복사 후 내부 전달(원본 수정 X) -> 맵, 슬라이스 등은 참조 전달(자동 포인터형 느낌)
+	//리시버(구조체)도 마찬가지로 포인터를 활용해서 메소드 내에서 원본 수정 가능
+
+	//ex1
+	bs1 := shoppingBasket{5, 1000}
+	fmt.Println("bs1 : ", bs1)
+	fmt.Println(bs1.purchase())
+	bs1.rePurchase(5, 1000)
+	fmt.Println(bs1.purchase())
 
 }
