65aea8d74398e101209c2927594f52f80ef98504
jjmin321 <jjmin321@naver.com>
2019-11-02 20:29:13 +0900

    src

--- GO_Files/StudyBook/Interfacehard/interfacehard3.go
+++ GO_Files/StudyBook/Interfacehard/interfacehard3.go
@@ -5,17 +5,17 @@ import "fmt"
 func checkType(a interface{}) {
 	switch a.(type) {
 	case bool:
-		fmt.Println("This is bool", a)
+		fmt.Println("This is bool,", a)
 	case int, int8, int16, int32, int64:
-		fmt.Println("This is int", a)
+		fmt.Println("This is int,", a)
 	case float32, float64:
-		fmt.Println("This is float", a)
+		fmt.Println("This is float,", a)
 	case nil:
-		fmt.Println("This is nil", a)
+		fmt.Println("This is nil,", a)
 	case string:
-		fmt.Println("This is string", a)
+		fmt.Println("This is string,", a)
 	default:
-		fmt.Println("What is this type", a)
+		fmt.Println("This is not bool, int, float, nil, string!,", a)
 	}
 }
 
