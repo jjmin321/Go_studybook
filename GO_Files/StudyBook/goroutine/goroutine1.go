5e678c28df873e4236dce6335145aea34bb7fe8f
jjmin321 <jjmin321@naver.com>
2019-11-03 16:51:36 +0900

    src

--- GO_Files/StudyBook/goroutine/goroutine1.go
+++ GO_Files/StudyBook/goroutine/goroutine1.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func exe1() {
+	fmt.Println("exe1 function start!", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe1 function end", time.Now())
+}
+
+func exe2() {
+	fmt.Println("exe2 function start!", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe2 function end", time.Now())
+}
+
+func exe3() {
+	fmt.Println("exe3 function start!", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("exe3 function end", time.Now())
+}
+
 func main() {
 	//고루틴(goroutine)
 	//타 언어의 쓰레드(Thread)와 비슷한 기능
@@ -8,4 +31,10 @@ func main() {
 	//비동기적 함수 루틴 실행(매우 적은 용량 차지) -> 채널을 통한 통신 가능
 	//공유메모리 사용 시에 정확한 동기와 코딩 필요
 	//싱글루틴에 비해 항상 빠른 처리 결과는 아니다.
+
+	//멀티 고루틴(쓰레드) 장점과 단점
+	//장점 : 응답성 향상, 자원공유를 효율적으로 활용 사용, 작업이 분리되어 코드 간결
+	//단점 : 구현하기 어려움, 테스트 및 디버깅 어려움, 전체 프로세스의 사이드이펙트 , 성능 저하, 동기화 코딩 반드시 숙지
+	//      데드락(교착 상태)...
+
 }
