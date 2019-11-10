fe4b816d0d5b0d4969a642902df57a2cbf05912d
jjmin321 <jjmin321@naver.com>
2019-11-10 22:47:32 +0900

    part20-2

---
+++ GO_Files/StudyBook/Channelhard/channelex2.go
@@ -0,0 +1,28 @@
+//20-2
+
+package main
+
+import "fmt"
+
+func sum(cnt int) <-chan int {
+	sum := 0
+	tot := make(chan int)
+
+	for i := 1; i < cnt; i++ {
+		sum += i
+	}
+	tot <- sum
+
+	return tot
+}
+
+func main() {
+	//채널(channel)
+	//채널 또한 함수의 반환 값으로 사용 가능
+
+	//ex1
+	c := sum(100) //c := <-tot
+
+	fmt.Println("ex1 : ", <-c)
+
+}
