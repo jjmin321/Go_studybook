c063d1eb233c2ce6beb8d565f9739a8af6c7c17f
jjmin321 <jjmin321@naver.com>
2019-11-14 18:14:39 +0900

    part23-2

---
+++ GO_Files/StudyBook/error/error3.go
@@ -0,0 +1,20 @@
+//23-2
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	//에러 처리
+	//errors 패키지의 New 메소드를 활용한 에러 생성
+	//Errorf 보다 더 많이 사용
+
+	var err1 error = errors.New("Error occurred - 1")
+	err2 := errors.New("Error occurred - 2")
+
+	fmt.Println("error1 : ", err1)
+	fmt.Println("error2 : ", err2)
+}
