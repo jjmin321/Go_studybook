5509eb6f826d1ad91941beeed22c8483a4c2e5d9
jjmin321 <jjmin321@naver.com>
2019-11-14 18:08:15 +0900

    part23-2

---
+++ GO_Files/StudyBook/error/error2.go
@@ -0,0 +1,38 @@
+//23-2
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func notZero(n int) (string, error) { //메소드 리턴 값 error 타입 중요!
+	if n != 0 {
+		s := fmt.Sprint("Hello Golang : ", n)
+		return s, nil
+	}
+
+	return "", fmt.Errorf("%d을 입력했습니다. 에러 발생!", n)
+}
+
+func main() {
+	//에러 처리
+	//Errorf를 이용한 에러 처리
+
+	a, err := notZero(1)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(a)
+
+	b, err := notZero(0) //0넣고 테스트
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Fatal 이후 프로그램 종료 후 실행 중지
+	fmt.Println(b)
+	fmt.Println("End error Handling!")
+}
